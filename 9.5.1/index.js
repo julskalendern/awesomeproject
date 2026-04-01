a7cc37168064298e8431ace97ef069ed3c47b80f
Juls <testsendsay@bk.ru>
2026-04-01 10:48:22 +0300

    ticket 9.5.1

---
+++ 9.5.1/index.js
@@ -0,0 +1,16 @@
+ function createArr(str) {
+   return Array.from(str);
+}
+
+//console.log(createArr("test"));
+
+ function arrRever(Array) {
+   return Array.reverse();
+}
+
+//console.log(arrRever([1, 2, 3, 4, 5]));
+ function createStr(Array) {
+  return Array.toString();
+}
+
+console.log(createStr([1,2,3,'test',4,5]));
